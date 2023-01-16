bcea2c6819ee4ab1598d3ce68dfd1e5e27e894dd
Dev Patel <devsavli@gmail.com>
2023-01-16 16:42:59 -0500

    Delete NodeModules and Package-lock.json attempt 3

---
+++ build/precache-manifest.5ff9cd8efe6dbecbf37640a516cb2603.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7809979edc7914899a836a82d8a3cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03016319bbc81ef7ffd1",
+    "url": "/static/css/2.857fa345.chunk.css"
+  },
+  {
+    "revision": "52bcd1eb2718b7f703a9",
+    "url": "/static/css/main.7d190bc9.chunk.css"
+  },
+  {
+    "revision": "03016319bbc81ef7ffd1",
+    "url": "/static/js/2.3dd7a2ba.chunk.js"
+  },
+  {
+    "revision": "243a7d8bdffe1f42cc3f08a639e4cee5",
+    "url": "/static/js/2.3dd7a2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52bcd1eb2718b7f703a9",
+    "url": "/static/js/main.22dc1c2c.chunk.js"
+  },
+  {
+    "revision": "90b91e1f07aaf18dc3a4",
+    "url": "/static/js/runtime-main.0a453e37.js"
+  },
+  {
+    "revision": "67406b10c2e23b58ac3885d5e2ed74f3",
+    "url": "/static/media/Dev Patel.67406b10.jpeg"
+  },
+  {
+    "revision": "19c38a38550bbcdbeb74922cd0781ccf",
+    "url": "/static/media/Dhanush-Mohan.19c38a38.jpeg"
+  },
+  {
+    "revision": "3d8e0c91f729082ea990024182de9f6b",
+    "url": "/static/media/IG.3d8e0c91.png"
+  },
+  {
+    "revision": "9aad843fea4bfed590796f94c44a2b67",
+    "url": "/static/media/Yining-Zou.9aad843f.jpeg"
+  },
+  {
+    "revision": "0c73125b89de5ef5090e3ebfa8f117eb",
+    "url": "/static/media/copresbg.0c73125b.jpg"
+  },
+  {
+    "revision": "02225ca73f248fc98eefb78579042e2b",
+    "url": "/static/media/hardwarebg.02225ca7.jpg"
+  },
+  {
+    "revision": "4b35346e8ad36e043038b83a1862c25b",
+    "url": "/static/media/lakshya.4b35346e.jpeg"
+  },
+  {
+    "revision": "cfacca439018ef24f1e36c73f0f40777",
+    "url": "/static/media/logo.cfacca43.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "e8a7bc4b4cfc412cacdeefea0c49ae99",
+    "url": "/static/media/softwarebg.e8a7bc4b.jpg"
+  }
+]);
